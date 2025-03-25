ba40a669cc6428191ec4dac005f237ae7a2c4116
Kaprisonn3 <kasperprip@gmail.com>
2025-03-25 15:21:41 +0100

    Update quiz_frontend.py
    -- arbejde

--- quiz_frontend.py
+++ quiz_frontend.py
@@ -1,5 +1,5 @@
 from nicegui import ui
-import CiscoAT
+import quiz_backend
 
 class Index():
     def __init__(self, val=0):
@@ -14,7 +14,6 @@ class Answer():
         self.answer = ""
     def set(self, answer):
         self.answer = answer
-        print(answer)
     def get(self):
         return self.answer
 
@@ -29,7 +28,7 @@ class Player():
         self.score = 0
 
 # sets global variable for the memocard index and refreshes the general view
-def handle_next_question(index: Index, memocards: list[CiscoAT.Memocard], chosen_answer: Answer, player: Player):
+def handle_next_question(index: Index, memocards: list[quiz_backend.Memocard], chosen_answer: Answer, player: Player):
     memocard = memocards[index.get_index()]
     if memocard.is_correct_answer(chosen_answer.get()):
         player.increment()
@@ -39,37 +38,37 @@ def handle_next_question(index: Index, memocards: list[CiscoAT.Memocard], chosen
     index.increment()
     general_view.refresh()
 
-#TODO create a function that checks if memocard contains picture and displays it if True
-#TODO create a function that checks if answer is correct and updates player
-@ui.refreshable
-def score_board(player: Player, index: Index, total_questions: int):
-    with ui.card().style("background-color: lightgrey; color: white; font-family: Courier;"):
-        ui.label(f"Score: {player.get_score()}")
-        ui.label(f"Questions Answered: {index.get_index()}/{total_questions}")
-        ui.label(f"Total Questions: {total_questions}")
-
 #TODO: implement what to do if question has picture
 @ui.refreshable
-def general_view(memocards: list[CiscoAT.Memocard], chosen_answer: Answer, index: Index, player: Player):
+def general_view(memocards: list[quiz_backend.Memocard], chosen_answer: Answer, index: Index, player: Player):
     memocard = memocards[index.get_index()]
     with ui.row():
         # Question and answers
         with ui.column():
-            ui.label(memocard.question)
-            answer_list = memocard.get_answer_list()
-            radio = ui.radio(answer_list, value=answer_list[0], on_change=lambda: chosen_answer.set(radio.value))
-            ui.button("Next Question", on_click=lambda: handle_next_question(index, memocards, chosen_answer, player))
+            if memocard.get_is_last():
+                ui.image(memocard.picture)#.props("height= 400 px") #picture veeeeeeery smalll - 
+                ui.label("Game Over!")
+                ui.button("Play Again", on_click=lambda: player.reset_score()) #implement reset score and play again in handle_next_question
+            else:
+                if memocard.has_picture:
+                    ui.image(memocard.picture)
+                ui.label(memocard.question)
+                answer_list = memocard.get_answer_list()
+                radio = ui.radio(answer_list, value=answer_list[0], on_change=lambda: chosen_answer.set(radio.value))
+                ui.button("Next Question", on_click=lambda: handle_next_question(index, memocards, chosen_answer, player))
         
         # Scoreboard
+        #TODO create a function that checks if memocard contains picture and displays it if True
         with ui.card().style("background-color: lightgrey; color: white; font-family: Courier;"):
             ui.label(f"Score: {player.get_score()}")
             ui.label(f"Questions Answered: {index.get_index()}/{len(memocards)}")
             ui.label(f"Total Questions: {len(memocards)}")
 
+# TODO: handle what happens when the game is over, and the list of memocards is empty
 player = Player()
 chosen_answer = Answer()
-memocards = CiscoAT.generate_cards()
+memocards = quiz_backend.generate_cards()
 index = Index()
 general_view(memocards, chosen_answer, index, player)
 
-ui.run(native=True)
+ui.run(reload=True, native=True)

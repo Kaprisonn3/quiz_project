efcfa8d0d8cac993b782a2e63dff671e0e862c3b
Kaprisonn3 <kasperprip@gmail.com>
2025-03-26 15:00:47 +0100

    Update CiscoAT.py
    donkey

--- CiscoAT.py
+++ CiscoAT.py
@@ -9,22 +9,21 @@ class Memocard:
         self.incorrect_answers = incorrect_answers
         self.picture = picture
         self.is_last = is_last
-        # answer_list is a randomized list of possible answers
         self.answer_list = [self.correct_answer] + self.incorrect_answers
+        # randomize answer list
         shuffle(self.answer_list)
         self.has_picture = False if picture == '*' else True
     # returns list of possible answers.
     # if no incorrect answers are given, (i.e. it is an actuual memocard) this returns a list with only the correct answer.
     def get_answer_list(self):
         return self.answer_list
-    # returns true if user_answer == answer
     def is_correct_answer(self, answer):
         return answer == self.correct_answer
     def get_is_last(self):
         return self.is_last
     def __str__(self):
         answers = "\n".join([f"{i+1}: {answer}" for i, answer in enumerate(self.answer_list)])
-        return f"Question: {self.question}\n{answers}\nid: {self.id}"
+        return f"Question: {self.question}\n{answers}\nid: {self.id}\nhas_picture: {self.has_picture}"
 
 # takes a csv file with a question on each line and possible answers seperated by comma, the first possible answer has to be the correct one
 # parameter csv_file = "input_questions_and_answers_cisco.csv", if nothing else is specified.
@@ -32,19 +31,17 @@ class Memocard:
 def generate_cards(csv_file="input_questions_and_answers_cisco.csv") -> list[Memocard]:
     memocards = []  # init list for storing all memocards read from .csv file
     id = 0
-    with open(csv_file, newline='') as questions_and_answers:
+    with open(csv_file, newline='', encoding='utf-8') as questions_and_answers:
         memocard_generator = csv.reader(questions_and_answers)
-        for row in memocard_generator:
-            question = row[0]
-            correct_answer = row[1]
-            # All entries between the correct answer and the last entry are incorrect answers
-            incorrect_answers = row[2:-1]
-            # Check if the last entry is a picture or asetrisk '*' which indicates no picture.
-            picture = row[-1]
-            memocards.append(Memocard(id, question, correct_answer, incorrect_answers, picture))
-            id += 1
-    # Add a last question to indicate that the game is over
-    memocards.append(Memocard(-1,"LAST QUESTION TEXT", "", picture="quiz_done.jpg", is_last=True)) #TODO: ./quiz_done??? eller sådan her
+        for id, row in enumerate(memocard_generator):
+            if len(row) >= 2:  # Ensure at least a question and a correct answer exist
+                question = row[0].strip()
+                correct_answer = row[1].strip()
+                incorrect_answers = [ans.strip() for ans in row[2:-1]]
+                picture = row[-1].strip()
+                memocards.append(Memocard(id, question, correct_answer, incorrect_answers, picture))
+    # Adding last question to handle end of quiz and out-of-bounds errors
+    memocards.append(Memocard(-1,"LAST QUESTION TEXT", "", picture="quiz_done.jpg", is_last=True))
     return memocards 
 
 ################## FOLLOWING FUNCTIONS ARE FOR TESTING PURPOSES AND SHOULD BE REMOVED (or commented out;)) WHEN IMPLEMENTED IN FRONTEND ###################

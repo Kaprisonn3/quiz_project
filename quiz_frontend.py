456f905ad0d13a37bcbdf1dd21fc170b566209c4
Kaprisonn3 <kasperprip@gmail.com>
2025-03-26 15:15:46 +0100

    Update quiz_frontend.py
    small additional changes in comments and handling of last question

--- quiz_frontend.py
+++ quiz_frontend.py
@@ -19,15 +19,21 @@ class Answer():
     def get(self):
         return self.answer
 
+#TODO: implement player name and functionality to save scores of players somewhere.
 class Player():
     def __init__(self):
         self.score = 0
+        self.name = "" # add player name functionality
     def increment(self):
         self.score = self.score + 1
     def get_score(self):
         return self.score 
     def reset_score(self):
         self.score = 0
+    def set_player_name(self,name):
+        self.name = name
+    def get_player_name(self):
+        return self.name
 
 # sets global variable for the memocard index and refreshes the general view
 def handle_next_question(index: Index, memocard: quiz_backend.Memocard, chosen_answer: Answer | None, player: Player) -> None:
@@ -49,11 +55,12 @@ def handle_next_question(index: Index, memocard: quiz_backend.Memocard, chosen_a
 def general_view(memocards: list[quiz_backend.Memocard], chosen_answer: Answer, index: Index, player: Player):
     memocard = memocards[index.get_index()]
     with ui.row().style("border: 2px solid black; padding: 10px; margin: 10px;"):
-        # Question and answers
+        
+        # question, answers and picture:
         with ui.column().style("flex: 2; padding: 10px;"):
             if memocard.get_is_last():
                 ui.image(memocard.picture).style("width: 629px; height: 252px; object-fit: scale-down;")
-                ui.button("Play Again", on_click=lambda: handle_next_question(index,memocard,None,player)) #implement reset score and play again in handle_next_question
+                ui.button("Play Again", on_click=lambda: handle_next_question(index,memocard,None,player))
             else:
                 if memocard.has_picture:
                     ui.image(memocard.picture).style("width: 550px; height: 400px; object-fit: scale-down;")
@@ -63,7 +70,6 @@ def general_view(memocards: list[quiz_backend.Memocard], chosen_answer: Answer, 
                 ui.button("Next Question", on_click=lambda: handle_next_question(index, memocard, chosen_answer, player))
         
         # Scoreboard
-        #TODO create a function that checks if memocard contains picture and displays it if True
         with ui.card().style("background-color: lightgrey; color: white; font-family: Courier;"):
             ui.label(f"Score: {player.get_score()}")
             ui.label(f"Questions Answered: {index.get_index()}/{len(memocards)}")
